ee4e8a13a8e4b5846eeb2e8282789d1cc0599814
Your Name <zeeshanhaider612@gmail.com>
2024-02-24 12:51:59 +0500

    zeeshan/feature/picture model is added in models.py

--- app/models.py
+++ app/models.py
@@ -6,3 +6,8 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+class picture(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    picture=models.URLField(max_length=200)
+    datetime=models.DateTimeField(auto_now_add=True)
