6883590095dd2c3841d588ae6011419d63f48826
Your Name <zeeshanhaider612@gmail.com>
2024-02-24 15:50:59 +0500

    zeeshan/update/update: created UserSerializer class in serializer.py and used UserSerializer in UserSignup view class

--- app/views.py
+++ app/views.py
@@ -3,12 +3,26 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView
 from app.models import User
 from django.urls import reverse_lazy
+from app.serializers import UserSerializer
+from django.http import JsonResponse
 
 # Create your views here.
 
 class UserSignup(CreateView):
     model=User
+    serializer_class=UserSerializer
     success_url=reverse_lazy('user:signup')
+
+    def post(self, request, *args, **kwargs):
+        user_data = self.serializer_class(data=request.data)
+
+        if user_data.is_valid():
+
+            user_data.save()
+            return JsonResponse(user_data, status=201)
+        
+        else:
+            return JsonResponse(user_data.errors, status=400)
 
     def get_success_url(self) -> str:
         return self.success_url
