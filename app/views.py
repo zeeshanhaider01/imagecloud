4fff829a30d5285cfba8373df5ba6155b905a6bf
Your Name <zeeshanhaider612@gmail.com>
2024-02-24 23:31:29 +0500

    zeeshan/update/update: Usersignup now can check if the user already does not exist then UserSignup will create user in database.

--- app/views.py
+++ app/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.views import LoginView
 from app.models import User
 from django.urls import reverse_lazy
 from app.serializers import UserSerializer
+from app.serializers import LoginSerializer
 from django.http import JsonResponse
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -18,8 +20,8 @@ class UserSignup(CreateView):
 
         if user_data.is_valid():
 
-            user_data.save()
-            return JsonResponse(user_data, status=201)
+            if User.objects.get(username=user_data.validated_data["username"]).exists():
+                return JsonResponse({'error': "User with this username already exists"}, status=400)
         
         else:
             return JsonResponse(user_data.errors, status=400)
@@ -30,6 +32,19 @@ class UserSignup(CreateView):
 class UserLogin(LoginView):
     model=User
     success_url=reverse_lazy("user:userprofile")
+
+    def post(self, request, *args, **kwargs):
+        # although we are not using *args, **kwargs here in post method but 
+        # it is a good practice to maintain consistency
+        user_data=LoginSerializer(data=request.data)
+
+        if user_data.is_valid():
+            username = user_data.validated_data['username']
+            password = user_data.validated_data['password']
+
+            user=authenticate(request,username=username, password=password)
+            if user is not None:
+                pass
 
     def get_success_url(self) -> str:
         return self.success_url
