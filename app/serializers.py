a62451931e6453b0a0e68469098dd2a95a7ac6c0
Your Name <zeeshanhaider612@gmail.com>
2024-02-24 16:06:31 +0500

    zeeshan/feature/feature: created LoginSerializer in serializer.py

--- app/serializers.py
+++ app/serializers.py
@@ -5,6 +5,12 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
-        
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+
+class LoginSerializer(serializers.Serializer):
+
+    class Meta:
+        username=serializers.EmailField()
+        password=serializers.CharField()
