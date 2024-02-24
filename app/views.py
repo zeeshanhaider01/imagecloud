1b238bdf3d4b7bd4adf0b43943996d89898e8d59
Your Name <zeeshanhaider612@gmail.com>
2024-02-24 14:44:40 +0500

    zeeshan/feaure/feature: UserSignup and UserLoginView has been created

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView
+from app.models import User
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class UserSignup(CreateView):
+    model=User
+    success_url=reverse_lazy('user:signup')
+
+    def get_success_url(self) -> str:
+        return self.success_url
+    
+class UserLogin(LoginView):
+    model=User
+    success_url=reverse_lazy("user:userprofile")
+
+    def get_success_url(self) -> str:
+        return self.success_url
